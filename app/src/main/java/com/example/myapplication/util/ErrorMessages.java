bc0c0ceee93e3d5b4c86bc63f8f29b5163a6b8bb
Jose Ortale <jose.ortale@pzbp.co.uk>
2021-03-11 18:03:27 +0000

    ErrorMessages criated to organize error messages

---
+++ app/src/main/java/com/example/myapplication/util/ErrorMessages.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.util;
+
+public class ErrorMessages {
+    public static final String MISSING_MIN_HEIGHT = "Missing minimum height.";
+    public static final String MISSING_MAX_HEIGHT = "Missing maximum height.";
+    public static final String INVALID_MAX_HEIGHT = "Maximum height should be greater than minimum height.";
+    public static final String INVALID_MIN_HEIGHT = "Minimum height should be greater than maximum height.";
+}
